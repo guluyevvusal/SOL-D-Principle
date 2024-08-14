ca36c88a9b0ba8fa1de1c7c6b5b21f6876405631
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-08-14 22:14:07 +0400

    Liskov Substitution Principle

---
+++ Liskov Substitution Principle/Liskov Substitution Principle/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿namespace Liskov_Substitution_Principle
+{
+    public abstract class Vehicle
+    {
+        public abstract void StartEngine();
+    }
+
+    public class Car : Vehicle
+    {
+        public override void StartEngine()
+        {
+            Console.WriteLine("Car engine started.");
+        }
+    }
+
+    public class ElectricCar : Vehicle
+    {
+        public override void StartEngine()
+        {
+            Console.WriteLine("Electric car started.");
+        }
+    }
+
+    public class Driver
+    {
+        public void Drive(Vehicle vehicle)
+        {
+            vehicle.StartEngine();
+        }
+    }
+}
