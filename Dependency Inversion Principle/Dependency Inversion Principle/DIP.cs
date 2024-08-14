1e7d201375f82780ed0780ea532e44d5a1d6c23f
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-08-14 22:20:07 +0400

    Dependency Inversion Principle

---
+++ Dependency Inversion Principle/Dependency Inversion Principle/DIP.cs
@@ -0,0 +1,39 @@
+﻿namespace Dependency_Inversion_Principle
+{
+    public interface IDataAccessLayer
+    {
+        void SaveData(string data);
+    }
+
+    public class SqlDataAccessLayer : IDataAccessLayer
+    {
+        public void SaveData(string data)
+        {
+
+            Console.WriteLine("Data is saved in SQL archives.");
+        }
+    }
+
+    public class FileDataAccessLayer : IDataAccessLayer
+    {
+        public void SaveData(string data)
+        {
+            Console.WriteLine("Data saved to file.");
+        }
+    }
+
+    public class DataManager
+    {
+        private readonly IDataAccessLayer dataAccessLayer;
+
+        public DataManager(IDataAccessLayer dataAccessLayer)
+        {
+            this.dataAccessLayer = dataAccessLayer;
+        }
+
+        public void SaveData(string data)
+        {
+            this.dataAccessLayer.SaveData(data);
+        }
+    }
+}
