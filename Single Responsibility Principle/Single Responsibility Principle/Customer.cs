0682a95845a2f351899b46eb2a705b1f0ceb72ca
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-08-14 22:00:58 +0400

    Add project files.

---
+++ Single Responsibility Principle/Single Responsibility Principle/Customer.cs
@@ -0,0 +1,33 @@
+﻿namespace Single_Responsibility_Principle
+{
+    public class Customer
+    {
+        private readonly string firstName;
+        private readonly string lastName;
+        private readonly string email;
+        private readonly string phone;
+
+        public Customer(string firstName, string lastName, string email, string phone)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.phone = phone;
+        }
+
+        public string GetFullName()
+        {
+            return $"{this.firstName} {this.lastName}";
+        }
+
+        public string GetPhoneNumber()
+        {
+            return this.phone;
+        }
+
+        public string GetEmailAddress()
+        {
+            return this.email;
+        }
+    }
+}
